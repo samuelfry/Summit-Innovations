d382dff87f5798dc9acafe527112ee5aefe40f0f
Samuel_Fry <samsamtheman108@gmail.com>
2026-03-16 17:32:41 -0600

    Created App Endpoint Connections

--- sketches/Prototype_Rev_9/wifi_connect.h
+++ sketches/Prototype_Rev_9/wifi_connect.h
@@ -9,11 +9,34 @@ int reconnect(const char* ssid, const char* password) {
   return WiFi.begin(ssid, password);
 }
 
-int database_con(const String& path, const char* header_type, const char* header, const String& body){
+int send_mes(const String& path, const char* header_type, const char* header, const String& mes){
   HTTPClient http;
   http.begin(path);
   http.addHeader(header_type, header);
-  int code = http.PUT(body);
+  int code = http.PUT(mes);
   http.end();
   return code;
+}
+
+bool listen(const String& path){
+  //Find path name
+  HTTPClient http;
+  http.begin(path);
+  int httpCode = http.GET();
+  // Serial.print("PingFromApp response: ");
+  // Serial.println(httpCode);
+  String payload;
+  if (httpCode) {
+    payload = http.getString();
+    // Serial.print("Payload: ");
+    // Serial.println(payload);
+  }
+  http.end();
+  //Sense if endpoint has change
+  if (payload == "\"pressed\"") {
+    return true;
+  }
+  else {
+    return false;
+  }
 }
